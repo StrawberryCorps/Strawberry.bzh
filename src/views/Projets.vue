f5724e2763ada4a775ea9b951bb41b5eb9a60dd1
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-03-07 23:04:45 +0100

    Add troll svg in projects

--- src/views/Projets.vue
+++ src/views/Projets.vue
@@ -1,6 +1,20 @@
 <template>
   <section>
     <header-page description="Tu trouveras ici la liste des différents projets que nous avons pu réaliser !" titre="Nos projets"></header-page>
+    <v-row>
+      <v-col cols="6">
+        <v-img
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            class="align-center"
+            src="https://raw.githubusercontent.com/StrawberryCorps/.github/main/github-metrics.svg" alt="GitHub"/>
+      </v-col>
+      <v-col cols="6">
+        <v-img
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            class="align-center"
+            src="https://raw.githubusercontent.com/StrawberryCorps/.github/main/metrics.plugin.pagespeed.detailed.svg" alt="GitHub"/>
+      </v-col>
+    </v-row>
   </section>
 </template>
 
